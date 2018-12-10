c5a8ac4a5348f7dce9bec644db1691ef2f1a4ef0
T3NY0M <39965669+T3NY0M@users.noreply.github.com>
2018-12-10 09:23:40 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+
+$ticket = "sHp2r312otmCpnGns3mCqJhovNiBh3zRu9B5arOTgpK9eqGjfnbE3IK2fWCzdXlmhXqn23-Ke5WumH6lv9uC3K2jra6KZM3XjZSKo7-Ik2ubfrCUi4Rzo";
+$meid = "8dc4238f238f30aa";
+$os="android";
+$sign="ace73d0d13c620252fa3d38ed9fc704dbf374d15e8b04c22aa9ba32ff808778a";
